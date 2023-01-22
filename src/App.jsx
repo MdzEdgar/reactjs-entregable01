1efbdad1e408cffa761d98014804e0ec470ec5c4
Edgar Méndez <mdzredgar@gmail.com>
2023-01-22 13:38:56 -0600

    Funcion random creada.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import db from "./db/quotes.json"
 
 function App() {
+  const getRandom = (arrayElements) => {
+    const quantityValues =arrayElements.length
+    const randomIndex = Math.floor(Math.random() * quantityValues)
+    return arrayElements[randomIndex]
+  }
 
   return (
     <div className="App">
