7d27777c95bb222b829dc8acacf74730df12916b
Edgar Méndez <mdzredgar@gmail.com>
2023-01-22 20:33:48 -0600

    Estilos finalizados

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,10 +3,10 @@ import QuoteButton from './QuoteButton'
 
 const QuoteBox = ({quote, newQuote, color}) => {
   return (
-    <div style={{color: color}}>
-      <p>{quote.quote}</p>
-      <h3>{quote.author}</h3>
-      <i className='bx bxs-quote-alt-left'></i>
+    <div className='quoteBox' style={{color: color}}>
+      <p className='quoteBox__quote'>{quote.quote}</p>
+      <h3 className='quoteBox__author'>{quote.author}</h3>
+      <i className='quoteBox__icon bx bxs-quote-alt-left'></i>
       <QuoteButton color={color} newQuote={newQuote} />
     </div>
   )
--- src/components/QuoteButton.jsx
+++ src/components/QuoteButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const QuoteButton = ({newQuote, color}) => {
   return (
-    <button onClick={newQuote} style={{backgroundColor: color}} >
+    <button className='quoteBox__button' onClick={newQuote} style={{backgroundColor: color}} >
       <i className='bx bx-refresh'></i>
     </button>
   )
